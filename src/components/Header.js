e56bb2836aa3f71e5890fcac936d0d62b7bdcf69
seungah yoo <sayoo0704@gmail.com>
2019-07-13 16:37:30 +0900

    tmp commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render () {
     return (
       <div className="container">
-        메인
+        Header
       </div>
     );
   }
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { FormCard, Nav,Header } from 'components'
+import 'styles/Upload.scss'
 
-class Upload extends Component {
-  render () {
+export default class Upload extends Component {
+  state = {
+    tags: [],
+    selectedTags: [],
+  }
+
+  render() {
     return (
       <div className="upload-container">
-        <h2>업로드화면</h2>
-     
+        <Nav />
+        <Header/>
+        <form className="upload">
+          <FormCard title="당신이 찾는 Mate">
+            <div className="basic-info">
+              
+              
+            </div>
+          </FormCard>
+          <FormCard title="글 설정" >
+            
+            <div className="selected-tag-group">
+              <div className="selected-tag"></div>
+            </div>
+          </FormCard>
+          <button>글 올리기</button>
+        </form>
       </div>
-    );
+    )
   }
 }
-
-export default Upload;
