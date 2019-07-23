c3915437b08ecf97f161f625a3bd084cfe423169
Shim MunSeong <puterism.k@gmail.com>
2019-07-23 19:38:10 +0900

    로그아웃 기능 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,9 @@ import { Notification } from 'components'
 import logo from 'assets/img/MateFitLogo-Horizonal.png'
 import bell from 'assets/img/bell.png'
 import bellActive from 'assets/img/bell_active.png'
+import { useAuth } from 'contexts/auth';
 
-export default class Nav extends Component {
+class Nav extends Component {
   state = {
     isNotiOpened: false,
     isNotiExists: false,
@@ -20,7 +21,7 @@ export default class Nav extends Component {
   
   render() {
     const { isNotiOpened, isNotiExists } = this.state
-    const { signup } = this.props
+    const { signup, logout } = this.props
     return (
       <nav className="nav">
         <div className="nav-container">
@@ -46,7 +47,7 @@ export default class Nav extends Component {
               <Link to="/mypage">
                 <li className="nav-menu-item">마이페이지</li>
               </Link>
-              <Link to="/">
+              <Link to="/" onClick={logout}>
                 <li className="nav-menu-item">로그아웃</li>
               </Link>
             </ul>
@@ -56,3 +57,5 @@ export default class Nav extends Component {
     )
   }
 }
+
+export default useAuth(Nav)
